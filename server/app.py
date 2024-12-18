f4fe3da04a56a5798ae081b97f5120f060e2d254
michaelanbrown <m_brownbrown@yahoo.com>
2024-12-18 10:12:57 -0500

    added a 202 status code to the index route

--- server/app.py
+++ server/app.py
@@ -14,7 +14,8 @@ def index():
     appname = current_app.name
     return f'''<h1>The host for this page is {host}</h1>
             <h2>The name of this application is {appname}</h2>
-            <h3>The path of this application on the user's device is {g.path}</h3>'''
+            <h3>The path of this application on the user's device is {g.path}</h3>''', \
+            202
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
